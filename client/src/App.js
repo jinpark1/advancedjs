8eb5cbd6b04858f333116862924b39c7688521b0
unknown <jempsey@gmail.com>
2018-07-04 14:41:50 -0700

    promise

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,97 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import request from 'superagent';
+
+function turnCallbackIntoPromise(fn) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, response) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(response);
+      })
+    })
+  }
+}
+
+function superAgentGet(url, callback) {
+  return request.get(url).end(callback)
+}
+
+function turnCallbackIntoPromisePractice(fn) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, response) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(response);
+      })
+    })
+  }
+}
+
+function doRequest(url) {
+  return new Promise((resolve, reject) => {
+    request.get(url).end((err, response) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(response);
+    })
+  })
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      counter: 0,
-      counter2: 2,
+      message: 'hello, happy 4th',
+      messagePractice: '',
+      messagePracticeMass: '',
+      messagePracticeSkinColor: '',
     };
   }
 
-  increment = () => {
-    // this.setState({ counter: this.state.counter + 2})
-    // console.log('1')
-    // this.setState({ counter: this.state.counter + 1})
-    // console.log('2')
-    // this.setState( prevState => {
-    //   console.log('first')
-    //   return { counter: prevState.counter + 1 }
-    // })
+  componentDidMount() {
+    const superAgentGetWithPromise = turnCallbackIntoPromise(superAgentGet);
+    superAgentGetWithPromise('https://swapi.co/api/people/112edafdf').then(response => {
+      this.setState({ message: 'The data from swapi is: ' + response.body.name });
+    }).catch(error => {
+      this.setState({ message: 'Got an error: ' + error.message });
+    });
 
-    // this.setState( prevState => {
-    //   console.log('second')
-    //   return { counter: prevState.counter + 1 }
-    // })
+    const superAgentGetwithPromisePractice = turnCallbackIntoPromisePractice(superAgentGet);
+    superAgentGetwithPromisePractice('https://swapi.co/api/people/3').then(response => {
+      console.log('practice--------', response.body)
+      this.setState({ messagePractice: 'The data from swapi is ' + response.body.name });
+      this.setState({ messagePracticeMass: response.body.mass });
+      this.setState({ messagePracticeSkinColor: response.body.skin_color });
+    }).catch(error => {
+      this.setState({ messagePractice: 'Got an error: '  + error.message });
+    });
 
-    this.setState( prevState => ({ counter: prevState.counter + 1 }))
-
-    // this.setState({ counter: this.state.counter + 1 })
-    // this.incrementBy5()
-  }
-
-  incrementBy5 = () => {
-    this.setState({ counter: this.state.counter + 5 })
-    this.setState({ counter2: this.state.counter2 + 5 })
   }
   
-
   render() {
     return (
       <div className="App">
-        <button onClick={this.increment}>Increment Button</button>
-        <div>-----------------</div>
-        <button onClick={this.incrementBy5}>Increment2 Button</button>
-        <div>counter: {this.state.counter}</div>
-        <div>--------------------</div>
-        <div>counter2: {this.state.counter2}</div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>Message: {this.state.message}</div>
+        <div>MessagePractice: {this.state.messagePractice}</div>
+        <div>Mass: {this.state.messagePracticeMass}</div>
+        <div>Skin Color: {this.state.messagePracticeSkinColor}</div>
       </div>
     );
   }
