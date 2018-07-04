560909660ac5ba0ac69fa5e12a171913966f6741
unknown <jempsey@gmail.com>
2018-07-04 11:23:51 -0700

    error handler practice

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function double(x) {
-    if(!Number.isInteger(x)){
-        throw new Error(`${x} is not an integer!`)
+    if (!Number.isInteger(x)) {
+      throw new Error(`${x} is not an integer!`)
     }
     return x * 2;
-}
-
-const result = double(5)
-
-console.log('-------- result', result);
+  }
+  
+  function plus5(x) {
+    return double(x + 5);
+  }
+  
+  let result
+  try {
+    plus5('bryson')
+  } catch (error) {
+    console.log('WE CAUGHT THE ERROR!', error)
+  }
+  
+  console.log('-------------- result', result);
--- instructor-code/index.js
+++ instructor-code/index.js
@@ -6,9 +6,46 @@ function double(x) {
 }
 
 function plus5(x) {
-  return x + 5;
+  return double(x + 5);
 }
 
-const result = plus5(double('bryson'))
+let result
+try {
+  plus5('bryson')
+} catch (error) {
+  console.log('WE CAUGHT THE ERROR!', error)
+}
 
 console.log('-------------- result', result);
+
+
+
+
+
+// function sendEmailToUser(email) {
+//   if (!axios.makeEmailCall()) {
+//     throw new Error('Failed to send email')
+//   }
+// }
+
+// function submit() {
+//   // const email = // data from form;
+//   let fileContents;
+//   let fileHandle;
+//   try {
+//     fileHandle = fs.openFile('/usr/local/bin/asdf')
+//     try {
+//       fileContents = fileHandle.readData()
+//     } catch (error) {
+  
+//     }
+//   } catch (error) {
+//     this.setState({ errorMessage: "Tried to send email but it failed" })
+//   }
+  
+//   try {
+//     doOtherThingWithFile(fileHandle)
+//   } catch (error) {
+
+//   }
+// }
